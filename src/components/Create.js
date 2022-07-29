6103758d34b6dee5e5cccb1c9331b549c2db1ce8
Atharva <scifacts001@gmail.com>
2022-07-29 14:52:56 +0530

    aa

---
+++ src/components/Create.js
@@ -0,0 +1,64 @@
+import axios from 'axios';
+import React, { useState } from 'react'
+import {useNavigate} from 'react-router-dom'
+
+
+const Create = () => {
+
+
+    const [name,setName]=useState('');
+    const [email,setEmail]=useState('');
+    const [image,setImage]=useState('');
+    
+    const history = useNavigate();
+    
+    const header={"Access-Control-Allow-Origin":"multipart/form-data"}
+
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+          console.log("Clicked")
+        axios.post(
+            'https://62e39bc63c89b95396cbfcf6.mockapi.io/crud-op',
+             {
+                name:name,
+                email:email,
+                image:image,
+                header
+            })
+
+            .then(()=>{
+              history("/read")
+            })
+    }
+  return (
+    <>                                        
+
+    <div className='container'>
+    <h1>Create User</h1>
+<form>
+  <div className="mb-3">
+    <label for="exampleInputEmail1" className="form-label">Name</label>
+    <input type="text" className="form-control" onChange={(e)=> setName(e.target.value)}/>
+    
+  </div>
+  <div className="mb-3">
+    <label for="exampleInputPassword1" className="form-label">Email</label>
+    <input type="email" className="form-control" onChange={(e)=> setEmail(e.target.value)} />
+  </div>
+
+  <div className="mb-3">
+  <label for="formFile" className="form-label">Upload Images</label>
+  <input className="form-control" type="file" onChange={(e)=> setImage(URL.createObjectURL(e.target.files[0]))}/>
+  <img src={image}/>
+</div>
+  
+  <button type="submit" className="btn btn-primary" onClick={handleSubmit}>Submit</button>
+</form>
+</div>
+
+
+    </>
+  )
+}
+
+export default Create
---
+++ src/components/Read.js
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const Read = () => {
+
+    const [data, setdata] = useState([])
+
+    function getData() {
+        
+        axios.get("https://62e39bc63c89b95396cbfcf6.mockapi.io/crud-op")
+            .then((res) => {
+                console.log(res);
+                setdata(res.data)
+            });
+
+            
+    }
+
+    function handleDelete(id) {
+        axios.delete(`https://62e39bc63c89b95396cbfcf6.mockapi.io/crud-op/${id}`)
+        .then(()=>{
+            getData();
+        });
+    }
+
+    useEffect(() => {
+        
+        getData();
+    }, [])
+
+
+
+
+    return (
+        <>
+
+            <div className='c'>
+                <h2>Read</h2>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Image</th>
+                            <th scope="col">Edit Data</th>
+                            <th scope="col">Delete Data</th>
+
+                        </tr>
+                    </thead>
+                    {
+                        data.map((eachData) => {
+                            return (
+                                <>
+
+                                    <tbody>
+                                        <tr>
+                                            <th scope="row">{eachData.id}</th>
+                                            <td>{eachData.name}</td>
+                                            <td>{eachData.email}</td>
+                                            <td><img src={eachData.image} /></td>
+                                            <td><button className='btn-light'>Edit</button></td>
+                                            <td><button className='btn-danger' onClick={()=>handleDelete(eachData.id)}>Delete</button></td>
+
+
+                                        </tr>
+
+
+                                    </tbody>
+                                </>
+                            )
+                        })
+                    }
+                </table>
+            </div>
+
+        </>
+    )
+}
+
+export default Read
